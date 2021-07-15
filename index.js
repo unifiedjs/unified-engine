b856d4362ef408fbdcf82ee8b4dad4f10986c726
stefanprobst <stefan.probst@univie.ac.at>
2021-07-15 16:14:12 +0200

    Add export of `Callback` type
    Closes GH-53.
    
    Reviewed-by: Christian Murphy <christian.murphy.42@gmail.com>
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
  * @typedef {import('./lib/index.js').ConfigTransform} ConfigTransform
  * @typedef {import('./lib/index.js').Preset} Preset
  * @typedef {import('./lib/index.js').Options} Options
+ * @typedef {import('./lib/index.js').Callback} Callback
  */
 
 export {engine} from './lib/index.js'
