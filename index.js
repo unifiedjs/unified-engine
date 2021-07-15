e4c7a5d6540a1ac7476f1e321c592ee1612429b0
Titus Wormer <tituswormer@gmail.com>
2021-07-15 19:37:18 +0200

    Add export of `Context` type

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
  * @typedef {import('./lib/index.js').ConfigTransform} ConfigTransform
  * @typedef {import('./lib/index.js').Preset} Preset
  * @typedef {import('./lib/index.js').Options} Options
+ * @typedef {import('./lib/index.js').Context} Context
  * @typedef {import('./lib/index.js').Callback} Callback
  */
 
