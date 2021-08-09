deefb995aca940940520cb0fc8172e85c5c283fe
Titus Wormer <tituswormer@gmail.com>
2021-08-09 09:32:50 +0200

    Fix tests

--- test/color.js
+++ test/color.js
@@ -1,9 +1,10 @@
 import path from 'path'
 import test from 'tape'
-import figures from 'figures'
 import {noop} from './util/noop-processor.js'
 import {spy} from './util/spy.js'
 import {engine} from '../index.js'
+
+const cross = process.platform === 'win32' ? '×' : '✖'
 
 const fixtures = path.join('test', 'fixtures')
 
@@ -25,7 +26,7 @@ test('color', (t) => {
         '\u001B[4m\u001B[31mreadme.md\u001B[39m\u001B[24m',
         '  1:1  \u001B[31merror\u001B[39m  No such file or directory',
         '',
-        '\u001B[31m' + figures.cross + '\u001B[39m 1 error',
+        '\u001B[31m' + cross + '\u001B[39m 1 error',
         ''
       ].join('\n')
 
